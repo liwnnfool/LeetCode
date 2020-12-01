9f339d889ae2094822ff7da180ca8c4b81389ce2
liwnnfool <powersismaely62922@gmail.com>
2020-12-02 06:48:05 +0800

    #80

---
+++ Array/80removeDuplicatesⅡ.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+  if (nums.length < 3) return nums.length;
+  
+  let i = 0, count = 1;
+  
+  while (i < nums.length) {
+    if (nums[i] === nums[i + 1]) {
+      count++;
+    } else {
+      count = 1;
+    }
+    
+    if (count > 2) {
+      nums.splice(i--, 1);
+      count--;
+    }
+    
+    i++;
+  }
+  
+  return nums.length;
+};
+
+// 根据 #26 第二个解决办法
+var removeDuplicates = function(nums) {
+  let length = 0;
+  
+  for (let num of nums) {
+    if (length < 2 || num !== nums[length - 2]) {
+      nums[length] = num;
+      length++;
+    }
+  }
+  
+  return length;
+};
