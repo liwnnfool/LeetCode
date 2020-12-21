a5348359831d6bbfe2effc6916df00728543f745
liwnnfool <powersismaely62922@gmail.com>
2020-12-21 19:25:12 +0800

    #287 update note

--- Array/findDuplicate.js
+++ Array/findDuplicate.js
@@ -37,7 +37,6 @@ var findDuplicate = function(nums) {
  * The first node's value is 3, and treat the value as a pointer to the next node:
  * 3(nums[0]) -> 6(nums[3]) -> 4(nums[6]) -> 5(nums[4]) -> 2(nums[5]) -> 7(nums[2]) -> 1(nums[7])
  *                                           ↑(nums[1]) <-            <-            <- ↓
- * They meet!
  * 
  * First we can find where slow and fast meet;
  * Because it's a cycle, so I can find the beginning of cycle, and beginning means duplicate number
@@ -60,3 +59,25 @@ var findDuplicate = function(nums) {
   
   return slow
 };
+
+/**
+ * Summarization:
+ * 1. Simulate a circular single linked list with value as index
+ * 
+ * Index 0 1 2 3 4 5 6 7
+ * Value 3 5 7 6 1 2 1 4
+ * 
+ * circular single linked list:
+ * 3(init nums[0]) -> 6(nums[3]) -> 1(nums[6]) -> 5(nums[1]) -> 2(nums[5]) -> 7(nums[2]) -> 4(nums[7]) -> 1(nums[4])
+ *                                  ↑          <-            <-            <-            <-            <- ↓
+ * 
+ * 2. According to the linked list, find the fast and slow pointer meeting point
+ * 
+ * init: slow = nums[0], fast = nums[slow]
+ * loop: slow = nums[slow], fast = nums[nums[fast]]
+ * 
+ * fast = slow = 2, they meet
+ * 
+ * 3. The slow pointer starts from the head, the fast pointer starts from the location, one step at a time
+ *    Eventually they will meet at the beginning of cycle
+ */
